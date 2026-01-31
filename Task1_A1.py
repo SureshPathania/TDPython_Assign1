e79961b86685a6f734749bb945098b15faaa36d3
SureshPathania <surepathania@gmail.com>
2026-01-31 17:45:35 +0530

    commit

---
+++ Task1_A1.py
@@ -0,0 +1,12 @@
+# 1st program of 1st Assignment. Demonstrating a basic calculator.
+
+num1 = float(input("Enter first number : "))      #reading first number
+num2 = float(input("Enter second number : "))     #reading second number
+
+print()
+print("Addition :", num1 + num2)                  # performing addition and printing result
+print("Subtraction :", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+#end of program.
---
+++ Task2_A1.py
@@ -0,0 +1,9 @@
+#2nd program of 1st Assigment. Demonstrating greeting message.
+
+first_name = input("Enter your first name: ")   #input of first name
+last_name = input("Enter your last name: ")     #input of last name
+
+full_name = first_name + " " + last_name
+print("Hello,",full_name+"! Welcome to the Python program.")
+
+#end of program
